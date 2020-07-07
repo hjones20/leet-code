e8d89016d403787562c2486632b89fb0ba34f9b1
hjones20 <chulahoma1@gmail.com>
2020-07-06 20:28:25 -0400

    Code clean-up for last exercise

--- easy/remove-adjacent-dupes-in-string.py
+++ easy/remove-adjacent-dupes-in-string.py
@@ -16,20 +16,28 @@ is possible, so the final string is "ca".
 """
 
 
-def remove_duplicates(s):
+class Solution(object):
 
-    input_string = list(s)
-    result_list = list(input_string[0])
+    def removeDuplicates(self, S):
+        """
+        :type S: String
+        :rtype: String
+        """
+        input_string = list(S)
+        result_list = list(input_string[0])
 
-    for char in input_string[1:]:
-        if result_list and char == result_list[-1]:
-            result_list.pop()
-        else:
-            result_list.append(char)
+        for char in input_string[1:]:
+            if result_list and char == result_list[-1]:
+                result_list.pop()
+            else:
+                result_list.append(char)
 
-    return "".join(result_list)
+        return "".join(result_list)
 
 
-print(remove_duplicates("abbaca"))
+if __name__ == "__main__":
+
+    test_case = "abbaca"
+    print(Solution().removeDuplicates(test_case))
 
 
