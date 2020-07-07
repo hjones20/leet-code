8a18938a2d0b1cd23aa366aacd261425f1ae438a
hjones20 <chulahoma1@gmail.com>
2020-07-07 11:26:01 -0400

    Complete jewels in stones exercise

---
+++ easy/jewels-and-stones.py
@@ -0,0 +1,42 @@
+"""
+Directions:
+------------
+You're given strings J representing the types of stones that are jewels, and S representing the
+stones you have.  Each character in S is a type of stone you have.  You want to know how many of the
+stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are
+case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+-----------
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+
+Example 2:
+-----------
+Input: J = "z", S = "ZZ"
+Output: 0
+"""
+
+
+class Solution(object):
+
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: String
+        :type S: String
+        :rtype: Integer
+        """
+        jewels_in_stones = 0
+
+        for char in S:
+            if char in J:
+                jewels_in_stones += 1
+
+        return jewels_in_stones
+
+
+if __name__ == "__main__":
+    print(Solution().numJewelsInStones("aA", "aAAbbbb"))
+
