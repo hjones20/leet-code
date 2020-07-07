e3ad0215cf41d55a4fda129430e1373c5e4333a2
hjones20 <chulahoma1@gmail.com>
2020-07-06 20:21:51 -0400

    Complete remove adjacent dupes in string exercise

---
+++ easy/remove-adjacent-dupes-in-string.py
@@ -0,0 +1,35 @@
+"""
+Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and
+equal letters, and removing them. We repeatedly make duplicate removals on S until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed the
+answer is unique.
+
+Example 1:
+-----------
+Input: "abbaca"
+Output: "ca"
+Explanation:
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is
+the only possible move.  The result of this move is that the string is "aaca", of which only "aa"
+is possible, so the final string is "ca".
+"""
+
+
+def remove_duplicates(s):
+
+    input_string = list(s)
+    result_list = list(input_string[0])
+
+    for char in input_string[1:]:
+        if result_list and char == result_list[-1]:
+            result_list.pop()
+        else:
+            result_list.append(char)
+
+    return "".join(result_list)
+
+
+print(remove_duplicates("abbaca"))
+
+
