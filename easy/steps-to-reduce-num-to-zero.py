0529955dd73aae89a48b3f14376109afc0aea094
hjones20 <chulahoma1@gmail.com>
2020-07-08 12:08:33 -0400

    Complete steps to reduce number to zero exercise

---
+++ easy/steps-to-reduce-num-to-zero.py
@@ -0,0 +1,41 @@
+"""
+Directions:
+------------
+Given a non-negative integer num, return the number of steps to reduce it to zero. If the current
+number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+Example 1:
+-----------
+Input: num = 14
+Output: 6
+
+Explanation:
+-------------
+Step 1) 14 is even; divide by 2 and obtain 7.
+Step 2) 7 is odd; subtract 1 and obtain 6.
+Step 3) 6 is even; divide by 2 and obtain 3.
+Step 4) 3 is odd; subtract 1 and obtain 2.
+Step 5) 2 is even; divide by 2 and obtain 1.
+Step 6) 1 is odd; subtract 1 and obtain 0.
+"""
+
+
+class Solution(object):
+
+    def numberOfSteps(self, num):
+        """
+        :type num: Integer
+        :rtype: Integer
+        """
+        counter = 0
+
+        while num:
+            num = num // 2 if num % 2 == 0 else num - 1
+            counter += 1
+
+        return counter
+
+
+if __name__ == "__main__":
+
+    print(Solution().numberOfSteps(123))
