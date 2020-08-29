6ab3dad960b1cbd633de806931ab841761ca8f11
hjones20 <chulahoma1@gmail.com>
2020-08-29 19:47:13 -0400

    Fix docstring

--- easy/running-sum-1d-array.py
+++ easy/running-sum-1d-array.py
@@ -16,10 +16,6 @@ Example 2:
 Input: nums = [1,1,1,1,1]
 Output: [1,2,3,4,5]
 Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
-Example 3:
-
-Input: nums = [3,1,2,10,1]
-Output: [3,4,6,16,17]
 
 Constraints:
 -------------
